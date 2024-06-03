451626bd3f7e5e2b5b3bff451d889502fd38bef7
Antoine Chartier1 <antoinechartier.ca@gmail.com>
2024-06-03 11:39:31 -0400

    enlever form du layoutroot et creer une apge form.html

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -47,22 +47,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="fr">
-        <form name="contact" method="post" data-netlify='true' >
-    <input type="hidden" name="form-name" value="contact" />
-      <div>
-       <label htmlFor="name">Name</label>
-       <input type="text" id="name" name="name" />
-      </div>
-      <div>
-       <label htmlFor="email">Email</label>
-       <input type="email" id="email" name="email" />
-      </div>
-      <div>
-       <label htmlFor="message">Message</label>
-       <textarea id="message" name="message" ></textarea>
-      </div>
-      <button type="submit">Submit</button>
-    </form>
+
 
 
         {children}
