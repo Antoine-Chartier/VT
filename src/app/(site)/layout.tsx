1367d89c4efef3d1dc8852cc619b9eb7df2528f4
Antoine Chartier1 <antoinechartier.ca@gmail.com>
2024-06-09 13:53:17 -0400

    nettoyer pour enlever la tentative de custom theme switcher

--- src/app/(site)/layout.tsx
+++ src/app/(site)/layout.tsx
@@ -2,26 +2,19 @@ import Footer from "../../components/Footer";
 import Nav from "../../components/Nav";
 import { questrial } from "@/lib/fonts";
 import { Providers } from "@/app/providers";
-import {ThemeProvider} from "../../context/ThemeContext";
 
-
- const AppLayout =  ({
-  children,
-}: Readonly<{ children: React.ReactNode }>) => {
-  
-  
+const AppLayout = ({ children }: Readonly<{ children: React.ReactNode }>) => {
   return (
-    <body className={`${questrial.className} flex flex-col min-h-screen bg-main text-text-primary`}>
-      {/* <Providers> */}
-      <ThemeProvider>
-      <Nav />
-      </ThemeProvider>
-      {/* </Providers> */}
-        <main className="bg-inherit flex-grow">{children}</main>
+    <body
+      className={`${questrial.className} flex flex-col min-h-screen bg-main text-text-primary`}
+    >
+      <Providers>
+        <Nav />
+      </Providers>
+      <main className="bg-inherit flex-grow">{children}</main>
       <Footer />
     </body>
   );
-}
+};
 
 export default AppLayout;
-
