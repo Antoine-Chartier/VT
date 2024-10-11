7097d98610d895df76e00523f3f163699d4cdccc
Antoine Chartier1 <antoinechartier.ca@gmail.com>
2024-10-11 09:24:16 -0400

    corrigé sticky footer pour qu'il reste en bas meme quand le contenu de la page n'est pas plein

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 
 const Footer = () => {
     return (
-        <footer className="w-full p-20 flex flex-col sticky bottom-0 bg-slate-950 -z-20">
+        <footer className="h-full w-full p-20 flex flex-col mt-auto sticky bottom-0 -z-20 bg-slate-950">
             <p className="text-center text-sm text-white">
                 © {new Date().getFullYear()} Viva Tremblant - Tous droits réservés
             </p>
